413c5fdb91d596bb98b51b49c5e69f4a481818e9
Vital Wang <vital.wang@hotmail.com>
2023-08-16 23:34:22 +0200

    alt tab

--- keyboards/keebio/iris/keymaps/Vital7788/keymap.c
+++ keyboards/keebio/iris/keymaps/Vital7788/keymap.c
@@ -28,8 +28,10 @@
 #define LA_G2a MO(_GAMING2a)
 #define LA_G2b MO(_GAMING2b)
 
-const uint16_t PROGMEM tab[] = {KC_END, KC_SPC, COMBO_END};
-const uint16_t PROGMEM num[] = {KC_HOME, KC_BSPC, COMBO_END};
+#define ALT_TAB LALT(KC_TAB)
+
+const uint16_t PROGMEM tab[] = {KC_PGDN, KC_SPC, COMBO_END};
+const uint16_t PROGMEM num[] = {KC_PGUP, KC_BSPC, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(tab, KC_TAB),
@@ -54,13 +56,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_ESC,  _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, LAYERS,
+     KC_ESC,  KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  LAYERS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+     ALT_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LA_NUM,
+     KC_HOME, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_END,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGUP,          KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LGUI, MT_ESC,  KC_BSPC,                   KC_SPC,  LA_SYM,  KC_LALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
