ae7c083691ef786fbe10185728af5aea5554419b
Vital Wang <vital.wang@hotmail.com>
2024-04-15 18:22:35 +0200

    thumb chords

--- keyboards/keebio/iris/keymaps/Vital7788/keymap.c
+++ keyboards/keebio/iris/keymaps/Vital7788/keymap.c
@@ -34,13 +34,15 @@ enum layers {
 
 #define ALT_ESC LALT(KC_ESC)
 
-const uint16_t PROGMEM tab_r[] = {KC_BRIU, MT_SPC, COMBO_END};
-const uint16_t PROGMEM tab_l[] = {KC_BRID, MT_BSPC, COMBO_END};
+const uint16_t PROGMEM thumb_r[] = {KC_PGDN, MT_SPC, COMBO_END};
+const uint16_t PROGMEM thumb_l[] = {KC_PGUP, MT_BSPC, COMBO_END};
+const uint16_t PROGMEM thumb_lr[] = {MT_SPC, MT_BSPC, COMBO_END};
 const uint16_t PROGMEM mute[] = {KC_VOLD, KC_VOLU, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(tab_r, KC_TAB),
-    COMBO(tab_l, KC_TAB),
+    COMBO(thumb_r, KC_HOME),
+    COMBO(thumb_l, KC_END),
+    COMBO(thumb_lr, KC_TAB),
     COMBO(mute, KC_MUTE),
 };
 
@@ -64,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC,  TL_G(1), TL_G(2), TL_G(3), TL_G(4), KC_PSCR,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  LAYERS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     ALT_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_VOLD, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_VOLU,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BRID,          KC_BRIU, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGUP,          KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LGUI, MT_ESC,  MT_BSPC,                   MT_SPC,  MT_ENT,  KC_LALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -76,11 +78,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYMBOLS] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, XXXXXXX, KC_ASTR, KC_PLUS, KC_LCBR, KC_PIPE,                            KC_GRV,  KC_RCBR, KC_HASH, KC_PERC, XXXXXXX, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_EQL,  KC_QUOT, KC_MINS, KC_LPRN, KC_CIRC,                            KC_DLR,  KC_RPRN, KC_UNDS, KC_DQUO, KC_COLN, _______,
+     KC_BRID, KC_EQL,  KC_QUOT, KC_MINS, KC_LPRN, KC_CIRC,                            KC_DLR,  KC_RPRN, KC_UNDS, KC_DQUO, KC_COLN, KC_BRIU,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, KC_TILD, KC_EXLM, KC_LT,   KC_LBRC, KC_AMPR, _______,          _______, KC_QUES, KC_RBRC, KC_GT,   KC_AT,   KC_BSLS, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
